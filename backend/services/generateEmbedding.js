b38403b4982cea4219fdc6e7e6bd60b34d9d1bc1
Aditya Kumar <aditya8799singh@gmail.com>
2025-12-31 21:36:33 +0530

    fixed api

--- backend/services/generateEmbedding.js
+++ backend/services/generateEmbedding.js
@@ -1,23 +1,24 @@
 const axios = require("axios");
 
-const HF_API_URL =
-  "https://api-inference.huggingface.co/embeddings/sentence-transformers/all-MiniLM-L6-v2";
+const HF_EMBEDDING_URL =
+  "https://router.huggingface.co/hf-inference/embeddings/sentence-transformers/all-MiniLM-L6-v2";
 
 async function getEmbedding(text) {
   const response = await axios.post(
-    HF_API_URL,
+    HF_EMBEDDING_URL,
     {
-      inputs: text.slice(0, 3000), 
+      inputs: text.slice(0, 3000), // safe + optimal
     },
     {
       headers: {
         Authorization: `Bearer ${process.env.HF_API_KEY}`,
+        "Content-Type": "application/json",
       },
       timeout: 10000,
     }
   );
 
-  return response.data[0]; 
+  return response.data[0];
 }
 
 module.exports = { getEmbedding };
