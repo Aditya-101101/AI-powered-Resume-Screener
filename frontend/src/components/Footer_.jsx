c1b30d828c212ea26967682736d3f06cd6884311
Aditya Kumar <aditya8799singh@gmail.com>
2026-01-01 18:19:29 +0530

    style fixing

--- frontend/src/components/Footer_.jsx
+++ frontend/src/components/Footer_.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-    <footer className="w-full border-t bg-linear-to-br from-purple-400/40 via-cyan-400/40 to-green-500/40  text-center text-lg text-slate-500 py-3">
+    <footer className="w-full mt-auto border-t border-white shadow-sm shadow-gray-400 bg-linear-to-br from-purple-300/40 via-cyan-300/40 to-green-300/40  text-center text-lg text-slate-700 py-3">
         © {new Date().getFullYear()} • Crafted by Aditya 🚀
     </footer>
 );
