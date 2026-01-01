bee08053cab9833e1b34f5914304f596388c422d
Aditya Kumar <aditya8799singh@gmail.com>
2026-01-01 18:15:02 +0530

    footer styling

--- frontend/src/components/Footer_.jsx
+++ frontend/src/components/Footer_.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-    <footer className="w-full border-t text-center text-xs text-slate-500 py-3">
+    <footer className="w-full border-t bg-linear-to-br from-purple-400/40 via-cyan-400/40 to-green-500/40  text-center text-lg text-slate-500 py-3">
         © {new Date().getFullYear()} • Crafted by Aditya 🚀
     </footer>
 );
